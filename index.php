b63006c69c8cf14359b7073b4b676d5b4481ac38
s13jl8 <s13jl8@mail.ru>
2014-04-25 16:25:20 +0400

    2

--- index.php
+++
@@ -1,9 +0,0 @@
-<head>
-<script src="js.js"></script>
-</head>
-<body>
-<!--Ссылка для входа на сайт-->
-<a href="end.php" onClick="newWindow('yeap.php?tar=1')">Нажми</A>
-</body>
-<footer>
-</footer>
--- yeap.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Инициализируем сессии
-session_start();
-// Устанавливаем флаг в сессию
-$_SESSION['access'] = true;
-if ($_GET["tar"] == "1") {header('Location: text.php');}
-if ($_GET["tar"] == "2") {header('Location: link1.php');}
-if ($_GET["tar"] == "3") {header('Location: link2.php');}
-?>
